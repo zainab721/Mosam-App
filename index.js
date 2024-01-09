71ac27375688d550d01f1df3760f9245f14b0cc8
zainab721 <155131588+zainab721@users.noreply.github.com>
2024-01-09 01:00:18 -0600

    added some js

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ let date = new Date(response.data.time * 1000);
 let iconElement = Document.querySelector("#icon");
 
 
-
-
-
 cityElement.innerHTML = response.data.city;
 temperatureElement.innerhtml = Math.round(temperature); 
 descricriptionElement.innerhtml = response.data.condition.descricription;
@@ -21,4 +18,25 @@ humidityElemen.innerhtml ='${response.data.condition.humidity}%';
 humidityElemen.innerhtml ='${response.data.condition.humidity}%';
 windspeedElement.innerhtml = Document.querySelector = '${response.data.wind.speed}km/h';
 timeElement.innerhtml ='${date.getDay()}'; '${date.getHours()}';'${date.getMinutes()}';'${date.getseconds()}';
-iconElement.innerHTML =  `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+iconElement.innerHTML =  `<img src="${response.data.condition.icon_url}" class="weather-app-icon" 
+/>`;
+
+unction formatDate(date)
+{
+    let day = date.getDay();
+    let minutes = date.getMinutes();
+    let getHours = date.getHours();
+
+    let days =   [
+        "Sunday" ,   
+        "Sunday" ,   
+        "Monday" ,   
+        "Tuesday" ,   
+        "Wednsday" ,   
+        "Thursday" ,   
+        "Friday" ,  
+        "Saturday" ,  
+     ];
+    }
+
+    
